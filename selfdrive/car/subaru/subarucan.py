011e584a66a59cd78268e1bcaacacb7cb4d1fb4e
Bugsy <30946888+bugsy924@users.noreply.github.com>
2019-02-27 22:23:55 +1100

    added brake change bit

--- selfdrive/car/subaru/subarucan.py
+++ selfdrive/car/subaru/subarucan.py
@@ -10,17 +10,20 @@ def create_steering_control(packer, bus, car_fingerprint, idx, steer, checksum):
       "Checksum": checksum
     }
     return packer.make_can_msg("ES_LKAS", 0, values)
-def create_brake_control(packer, bus, car_fingerprint, brake, speed):
+
+  def create_brake_control(packer, bus, car_fingerprint, brake, speed, change):
   if car_fingerprint in (CAR.OUTBACK, CAR.LEGACY):
 
     values = {
       "Brake_Pedal": brake,
       "Speed": speed,
-      "NEW_SIGNAL_1": 1
+      "NEW_SIGNAL_1": 1,
+      "NEW_SIGNAL_2": change
     }
     return packer.make_can_msg("Brake_Pedal", 1, values)
 
-'''  if car_fingerprint == CAR.XV2018:
+'''
+if car_fingerprint == CAR.XV2018:
     values = {
       "Checksum": checksum,
       "Byte1": idx,
