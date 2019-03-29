7e0173baae5853909ac50f710618773145e985f1
Bugsy <bugsyborromeo@hotmail.com>
2019-03-29 17:50:52 +1100

    Update carcontroller.py

--- selfdrive/car/subaru/carcontroller.py
+++ selfdrive/car/subaru/carcontroller.py
@@ -2,7 +2,6 @@ from common.numpy_fast import clip, interp
 from common.realtime import sec_since_boot
 from selfdrive.config import Conversions as CV
 from selfdrive.boardd.boardd import can_list_to_can_capnp
-from selfdrive.car import apply_std_steer_torque_limits
 from selfdrive.car.subaru.carstate import CarState, get_powertrain_can_parser, get_eyesight_can_parser
 from selfdrive.car.subaru import subarucan
 from selfdrive.car.subaru.values import CAR, DBC
@@ -11,7 +10,7 @@ from selfdrive.can.packer import CANPacker
 
 class CarControllerParams():
   def __init__(self, car_fingerprint):
-    self.STEER_MAX = 2047                 # max_steer 2047
+    self.STEER_MAX = 2047                 # max_steer 4095
     self.STEER_STEP = 1                   # how often we update the steer cmd
     self.STEER_DELTA_UP = 30              # torque increase per refresh
     self.STEER_DELTA_DOWN = 30            # torque decrease per refresh
@@ -30,7 +29,6 @@ class CarController(object):
     self.lkas_active = False
     self.steer_idx = 0
     self.apply_steer_last = 0
-    self.last_blinker_frame = 0
     self.car_fingerprint = car_fingerprint
 
     # Setup detection helper. Routes commands to
@@ -54,16 +52,25 @@ class CarController(object):
     if (frame % P.STEER_STEP) == 0:
 
       final_steer = actuators.steer if enabled else 0.
-      apply_steer = apply_std_steer_torque_limits(final_steer*P.STEER_MAX, self.apply_steer_last, CS.steer_torque_driver, self.params)
+      apply_steer = final_steer * P.STEER_MAX
+      # limits due to driver torque
+      driver_max_torque = P.STEER_MAX + (P.STEER_DRIVER_ALLOWANCE + CS.steer_torque_driver * P.STEER_DRIVER_FACTOR) * P.STEER_DRIVER_MULTIPLIER
+      driver_min_torque = -P.STEER_MAX + (-P.STEER_DRIVER_ALLOWANCE + CS.steer_torque_driver * P.STEER_DRIVER_FACTOR) * P.STEER_DRIVER_MULTIPLIER
+      max_steer_allowed = max(min(P.STEER_MAX, driver_max_torque), 0)
+      min_steer_allowed = min(max(-P.STEER_MAX, driver_min_torque), 0)
+      apply_steer = clip(apply_steer, min_steer_allowed, max_steer_allowed)
+
+      # slow rate if steer torque increases in magnitude
+      if self.apply_steer_last > 0:
+        apply_steer = clip(apply_steer, max(self.apply_steer_last - P.STEER_DELTA_DOWN, -P.STEER_DELTA_UP), self.apply_steer_last + P.STEER_DELTA_UP)
+      else:
+        apply_steer = clip(apply_steer, self.apply_steer_last - P.STEER_DELTA_UP, min(self.apply_steer_last + P.STEER_DELTA_DOWN, P.STEER_DELTA_UP))
+
+      apply_steer = int(round(apply_steer))
       self.apply_steer_last = apply_steer
 
+
       lkas_enabled = enabled and not CS.steer_not_allowed
-
-      if CS.left_blinker_on or CS.right_blinker_on:
-        self.last_blinker_frame = frame
-
-      if (frame - self.last_blinker_frame) < 50:
-        lkas_enabled = False
 
       if not lkas_enabled:
           apply_steer = 0
