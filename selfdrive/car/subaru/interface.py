d4bae377fde4ab348847a0676f8834c3c047e565
Bugsy <30946888+bugsy924@users.noreply.github.com>
2019-07-27 14:04:14 +1000

    update outback tuning

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -71,11 +71,11 @@ class CarInterface(object):
       ret.centerToFront = ret.wheelbase * 0.5
       ret.steerRatio = 20           # learned, 14 stock
       tire_stiffness_factor = 1.0
-      ret.steerActuatorDelay = 0.2
-      ret.steerRateCost = 0.4
-      ret.lateralTuning.pid.kf = 0.00005
+      ret.steerActuatorDelay = 0.07
+      ret.steerRateCost = 0.7
+      ret.lateralTuning.pid.kf = 0.000043
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 10., 20.], [0., 10., 20.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.01, 0.05, 0.18], [0.003, 0.018, 0.025]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.01, 0.05, 0.2], [0.003, 0.018, 0.025]]
       ret.steerMaxBP = [0.] # m/s
       ret.steerMaxV = [1.]
 
