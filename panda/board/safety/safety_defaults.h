cbc84786b457c75a0d688d61d310f5e7cf9d0685
Bugsy <bugsyborromeo@hotmail.com>
2019-08-18 21:17:58 +1000

    force forwarding between 0 <-> 2 by default

--- panda/board/safety/safety_defaults.h
+++ panda/board/safety/safety_defaults.h
@@ -26,9 +26,17 @@ static int nooutput_tx_lin_hook(int lin_num, uint8_t *data, int len) {
 }
 
 static int default_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
-  UNUSED(bus_num);
   UNUSED(to_fwd);
-  return -1;
+  int bus_fwd = -1;
+  if (bus_num == 0) {
+    bus_fwd = 2;  // Camera CAN
+  }
+  if (bus_num == 2) {
+   bus_fwd = 0;  // Main CAN
+  }
+
+  // fallback to do not forward
+  return bus_fwd;
 }
 
 const safety_hooks nooutput_hooks = {
