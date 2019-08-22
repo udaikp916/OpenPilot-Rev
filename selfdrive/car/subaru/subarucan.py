ad88c43c3ede7bab9fde27cf654f3431d6efea2e
Bugsy <bugsyborromeo@hotmail.com>
2019-08-23 01:24:15 +1000

    adding back checksum, accidentally removed

--- selfdrive/car/subaru/subarucan.py
+++ selfdrive/car/subaru/subarucan.py
@@ -78,13 +78,11 @@ def create_es_lkas(packer, es_lkas_msg, visual_alert, left_line, right_line):
 
   return packer.make_can_msg("ES_LKAS_State", 0, values)
 
-def create_es_throttle_control(packer, fake_button, real_button, checksum_offset):
+def create_es_throttle_control(packer, fake_button, checksum, real_button, checksum_offset):
 
   values = copy.copy(es_accel_msg)
   if fake_button != 0 and real_button == 0:
     values["Checksum"] = (checksum + checksum_offset) % 256
     values["Button"] = fake_button
-    values["Standstill"] = standstill
-    values["Standstill_2"] = standstill
 
   return packer.make_can_msg("ES_CruiseThrottle", 0, values)
