4a2e0fe21d6649a788a152cd07b92e78f03bc35b
Ross Fisher <zorrobyte@gmail.com>
2019-09-21 10:03:14 -0400

    Cleanup and change rate
    Learns 0.0025 every second

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -67,20 +67,16 @@ class PathPlanner(object):
 
     # TODO: Check for active, override, and saturation
     if active and angle_steers - angle_offset > 3.:
-      self.curvature_offset_i -= self.LP.d_poly[3] / (60.0 * 20.0)
-      self.curvature_offset_i = clip(self.curvature_offset_i, -0.5,  0.5)
+      self.curvature_offset_i -= self.LP.d_poly[3] / 12000
       #self.LP.d_poly[3] += self.curvature_offset_i
-      self.frame += 1
-      if self.frame == 20:
-          pickle.dump( self.curvature_offset_i, open( "/data/curvature.p", "wb" ) )
-          self.frame = 0
     elif active and angle_steers - angle_offset < -3.:
-      self.curvature_offset_i += self.LP.d_poly[3] / (60.0 * 20.0)
-      self.curvature_offset_i = clip(self.curvature_offset_i, -0.5,  0.5)
-      self.frame += 1
-      if self.frame == 20:
-          pickle.dump( self.curvature_offset_i, open( "/data/curvature.p", "wb" ) )
-          self.frame = 0
+      self.curvature_offset_i += self.LP.d_poly[3] / 12000
+
+    self.curvature_offset_i = clip(self.curvature_offset_i, -0.3, 0.3)
+    self.frame += 1
+    if self.frame == 30000: #every 5 mins
+      pickle.dump(self.curvature_offset_i, open("/data/curvature.p", "wb"))
+      self.frame = 0
     # account for actuation delay
     self.cur_state = calc_states_after_delay(self.cur_state, v_ego, angle_steers - angle_offset, curvature_factor, VM.sR, CP.steerActuatorDelay)
 
